1f7a4b90df12b537f8164d4c346a020fbdc8130a
Ricky <rchip0812@gmail.com>
2019-09-13 13:37:51 -0400

    MaxHitCalc Additions

--- maxHitCalc.php
+++ maxHitCalc.php
@@ -178,35 +178,6 @@ if(isset($_SESSION['u_userID'])){
 			</div>
 		</div>
 
-
-		<div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
-
-			<div class="container-fluid text-center">
-				<h4>Current Gear</h4>
-				<div class="row">
-					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-						<img src="images/slot_images/Head_slot.png" id="headSlotImage"><br><div id="HeadSlot" value="0">None</div><br>
-						<img src="images/slot_images/Cape_slot.png" id="capeSlotImage"><br><div id="CapeSlot" value="0">None</div><br>
-						<img src="images/slot_images/Neck_slot.png" id="neckSlotImage"><br><div id="NeckSlot" value="0">None</div><br>
-						<img src="images/slot_images/Weapon_slot.png" id="weaponSlotImage"><br><div id="WeaponSlot" value="0">None</div><br>
-						<img src="images/slot_images/Body_slot.png" id="bodySlotImage"><br><div id="BodySlot" value="0">None</div><br>
-					</div>
-					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-						<img src="images/slot_images/Shield_slot.png" id="shieldSlotImage"><div id="ShieldSlot" value="0">None</div><br>
-						<img src="images/slot_images/Legs_slot.png" id="legsSlotImage"><div id="LegsSlot" value="0">None</div><br>
-						<img src="images/slot_images/Hands_slot.png" id="handsSlotImage"><div id="HandsSlot" value="0">None</div><br>
-						<img src="images/slot_images/Feet_slot.png" id="feetSlotImage"><div id="FeetSlot" value="0">None</div><br>
-						<img src="images/slot_images/Ring_slot.png" id="ringSlotImage"><div id="RingSlot" value="0">None</div><br>
-					</div>
-				</div>
-				
-			</div>
-			<div class="container text-center">
-				<div class="mt-1"><p class="d-inline">Current strength bonus: </p><p class="d-inline text-danger" id="currentStrengthBonus" value="0">0</p></div>
-				<button type="button" class= "btn-primary pl-5 pr-5" onclick="calculateMaxHit();">Calculate hit!</button><br>
-			</div>
-		</div>
-
 		<div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12">
 			<div class="container text-center">
 						
@@ -268,6 +239,58 @@ if(isset($_SESSION['u_userID'])){
 			
 			</div>
 		</div>
+
+		<div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+
+			<div class="container-fluid text-center">
+				<h4>Current Gear</h4>
+				<div class="row">
+					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
+						<img src="images/slot_images/Head_slot.png" id="headSlotImage"><br><div id="HeadSlot" value="0">None</div><br>
+						<img src="images/slot_images/Cape_slot.png" id="capeSlotImage"><br><div id="CapeSlot" value="0">None</div><br>
+						<img src="images/slot_images/Neck_slot.png" id="neckSlotImage"><br><div id="NeckSlot" value="0">None</div><br>
+						<img src="images/slot_images/Weapon_slot.png" id="weaponSlotImage"><br><div id="WeaponSlot" value="0">None</div><br>
+						<img src="images/slot_images/Body_slot.png" id="bodySlotImage"><br><div id="BodySlot" value="0">None</div><br>
+					</div>
+					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
+						<img src="images/slot_images/Shield_slot.png" id="shieldSlotImage"><div id="ShieldSlot" value="0">None</div><br>
+						<img src="images/slot_images/Legs_slot.png" id="legsSlotImage"><div id="LegsSlot" value="0">None</div><br>
+						<img src="images/slot_images/Hands_slot.png" id="handsSlotImage"><div id="HandsSlot" value="0">None</div><br>
+						<img src="images/slot_images/Feet_slot.png" id="feetSlotImage"><div id="FeetSlot" value="0">None</div><br>
+						<img src="images/slot_images/Ring_slot.png" id="ringSlotImage"><div id="RingSlot" value="0">None</div><br>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<div class="container-fluid pt-5 pb-3 border-top">
+		<div class="row">
+			<div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+				<div class="container text-center">
+					<div>
+						<p class="d-inline">Total strength bonus: </p>
+						<div class="d-inline text-danger" id="currentStrengthBonus" value="0">0</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+				<div class="container text-center">
+						<button type="button" class= "btn-primary pl-5 pr-5 pt-2 pb-2" onclick="calculateMaxHit();">Calculate hit!</button><br>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+				<div class="container text-center">
+						<div>
+						<h3 class="d-inline">Max melee hit:</h3> 
+						<img class="icon" src="images/Red_hitsplat.png">
+						<div class="d-inline maxHitText1Digit text-light" id="currentMaxHit">1</div>
+						</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
 </div>	
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -275,303 +298,7 @@ if(isset($_SESSION['u_userID'])){
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-<script type="text/javascript">
-
-function getRequirements(diary) {
-	table = "#table"+diary;
-	data ={diary: diary};
-
-	if(document.getElementById('radioChar1')){//Check if exists
-		if(document.getElementById('radioChar1').checked){
-	  		char = document.getElementById('radioChar1').value;
-			data ={diary: diary, char: char};
-		}
-	}	
-	if(document.getElementById('radioChar2')){//Check if exists
-		if(document.getElementById('radioChar2').checked){
-	  		char = document.getElementById('radioChar2').value;
-
-			data ={diary: diary, char: char};
-		}
-	}	
-	if(document.getElementById('radioChar3')){//Check if exists
-		if(document.getElementById('radioChar3').checked){
-	  		char = document.getElementById('radioChar3').value;
-			data ={diary: diary, char: char};
-		}
-	}	
-	if(document.getElementById('radioChar4')){//Check if exists
-		if(document.getElementById('radioChar4').checked){
-	  		char = document.getElementById('radioChar4').value;
-			data ={diary: diary, char: char};
-		}
-	}
-
-		$.ajax({
-			type: "POST",
-			url: "cluescroll/cluescrollRequirements.php",
-			data: data,
-			cache: false,
-
-			success: function(data) {
-			$(table).html(data);
-			},
-			error: function(xhr, ajaxOptions, thrownError) {
-				       alert(xhr.status);
-       				 alert(thrownError);
-			}
-		});
-	}
-
-function populateSlot(itemSlot,updateField) {
-	data = {itemSlot: itemSlot};
-	//updateField = '#itemSlotField';
-		$.ajax({
-			type: "POST",
-			url: "maxHitScripts/getSlotItems.php",
-			data: data,
-			cache: false,
-
-			success: function(data) {
-			$(updateField).html(data);
-			},
-			error: function(xhr, ajaxOptions, thrownError) {
-				       alert(xhr.status);
-       				 alert(thrownError);
-			}
-		});
-	}
-
-function getCharacterStrength() {
-	if (document.getElementById('radioChar1').checked) {
-		var characterName = document.getElementById('radioChar1').value;
-	}
-	else if (document.getElementById('radioChar2').checked) {
-		var characterName = document.getElementById('radioChar2').value;
-	}
-	else if (document.getElementById('radioChar3').checked) {
-		var characterName = document.getElementById('radioChar3').value;
-	}
-	else if (document.getElementById('radioChar4').checked) {
-		var characterName = document.getElementById('radioChar4').value;
-	}
-	
-	data = {characterName: characterName};
-	updateField = '#strengthLevelDiv';
-		$.ajax({
-			type: "POST",
-			url: "maxHitScripts/maxHitQueries.php",
-			data: data,
-			cache: false,
-
-			success: function(data) {
-			$(updateField).html(data);
-			},
-			error: function(xhr, ajaxOptions, thrownError) {
-				       alert(xhr.status);
-       				 alert(thrownError);
-			}
-		});
-	}
-
-function calculateMaxHit(){
-
-	strengthBonus = parseInt(document.getElementById('currentStrengthBonus').innerHTML);
-	strengthLevel = parseInt(document.getElementById('strengthLevel').value);
-	boost = document.getElementById('boost').value;
-	prayer = document.getElementById('prayer').value;
-	attackStyle = document.getElementById('attackStyle').value;
-	setBonus = parseFloat('1.00');
-	strengthLevelBool = false;
-	attackStyleBool = false;
-	specialAttack = parseFloat('1.00');
-
-	if(strengthLevel==''){
-		document.getElementById('strengthLevelMessage').innerHTML = 'Please enter a number 1-99';
-	}
-	else
-	{
-		if(isNaN(strengthLevel)){
-			document.getElementById('strengthLevelMessage').innerHTML = 'Please enter a number 1-99';
-		}	
-		else
-		{
-			if((strengthLevel > 0)&&(strengthLevel < 100)){//Valid Strength level
-				document.getElementById('strengthLevelMessage').innerHTML = "";
-				strengthLevelBool = true;
-			}
-			else{
-				document.getElementById('strengthLevelMessage').innerHTML = 'Strength must be between 1-99';
-			}
-		}
-	}
-
-	if(attackStyle =="Select style"){
-		$('#attackStyleMessage').removeClass('hidden');
-	}
-	else{
-		$('#attackStyleMessage').addClass('hidden');
-		attackStyleBool = true;
-		
-	}
-
-	if((attackStyleBool)&&(strengthLevelBool))//Good to calculate the max hit
-	{		
-		//Calculate potion bonus
-		if(boost=="None"){
-			boost = parseFloat('0.00');
-		}
-		else if((boost=="Strength potion")||(boost=="Combat potion")){
-			boost = parseFloat(strengthLevel*0.10)+parseFloat(3);
-		}
-		else if((boost=="Super strength potion")||(boost=="Super combat potion")||(boost=="Overload potion(Nightmare Zone)")){
-			boost = parseFloat(strengthLevel*0.15)+parseFloat(5);
-		}
-		else if(boost=="Zamorak brew"){
-		boost = parseFloat(strengthLevel*0.12)+parseFloat(2);
-		}
-		else if(boost=="Overload potion(-)"){
-			boost = parseFloat(strengthLevel*0.10)+parseFloat(4);
-		}
-		else if(boost=="Overload potion(Chambers of Xeric)"){
-			boost = parseFloat(strengthLevel*0.13)+parseFloat(5);
-		}
-		else if(boost=="Overload potion(+)"){
-			boost = parseFloat(strengthLevel*0.16)+parseFloat(6);
-		}
-		//alert('Boost is: '+ boost);
-			boost = parseInt(Math.floor(boost));
-		//alert('Rounded down boost is: '+ boost);
-
-		switch(attackStyle){//Convert attackStyle to integer value
-			case 'Accurate':
-			attackStyle = parseInt('0');
-			break;
-			case 'Aggressive':
-			attackStyle = parseInt('3');
-			break;
-			case 'Controlled':
-			attackStyle = parseInt('1');
-			break;
-			case 'Defensive':
-			attackStyle = parseInt('0');
-			break;
-		}
-
-		switch(prayer){//Convert prayer bonus to integer value
-			case 'None':
-			prayer = parseFloat('1.00');
-			break;
-			case 'Burst of Strength':
-			prayer = parseFloat('1.05');
-			break;
-			case 'Superhuman Strength':
-			prayer = parseFloat('1.10');
-			break;
-			case 'Ultimate Strength':
-			prayer = parseFloat('1.15');
-			break;
-			case 'Chivalry':
-			prayer = parseFloat('1.18');
-			break;
-			case 'Piety':
-			prayer = parseFloat('1.23');
-			break;
-		}		
-
-		//Calculate effective strength
-		effectiveStrength = parseFloat(Math.floor((strengthLevel+boost)*prayer*setBonus)+attackStyle);
-		alert('EffectiveStrength:'+effectiveStrength);
-		//Calculate the base damage
-		baseDamage= 1.3+(effectiveStrength/10)+(strengthBonus/80)+((effectiveStrength*strengthBonus)/640);
-		
-		maxHit= Math.floor(baseDamage);
-		alert(maxHit);
-		//Calculate bonus damage if using as special attack
-		//maxHit = Math.floor(baseDamage*specialAttack);
-	}
-
-}
-
-function updateTotalStrength(strengthBonus, itemSlot) {//Needs to be called after each item is added and completely recalculated in-case items are rechosen
-	totalStrength = document.getElementById('currentStrengthBonus').innerHTML;
-
-	if(itemSlot=='2h-weapon')//Cannot have a shield with a 2h weapon
-	{
-		if(document.getElementById('WeaponPlaceholder')){
-			strengthToSubtract = document.getElementById('WeaponPlaceholder').innerHTML;
-			strengthToSubtract = strengthToSubtract.substring(strengthToSubtract.indexOf(":")+3);
-			totalStrength = parseInt(totalStrength) - parseInt(strengthToSubtract);
-		}
-		if(document.getElementById('ShieldPlaceholder')){
-			subtractShieldBonus = document.getElementById('ShieldPlaceholder').innerHTML;
-			subtractShieldBonus = subtractShieldBonus.substring(subtractShieldBonus.indexOf(":")+3);
-			totalStrength = parseInt(totalStrength) - parseInt(subtractShieldBonus);
-		}
-		$('#ShieldSlot').text('Unavailable: 2 handed weapon');
-		$('#ShieldSlot').addClass('text-danger');
-	}
-
-	if(itemSlot=='Shield')//Cannot have a 2h weapon with a shield
-	{
-		$('#ShieldSlot').removeClass('text-danger');
-		if(document.getElementById('2h-weaponPlaceholder')){
-			subtract2HandedBonus = document.getElementById('2h-weaponPlaceholder').innerHTML;
-			subtract2HandedBonus = subtract2HandedBonus.substring(subtract2HandedBonus.indexOf(":")+3);
-			totalStrength = parseInt(totalStrength) - parseInt(subtract2HandedBonus);
-			$('#WeaponSlot').text('None');
-		}
-	}
-	if(itemSlot=='Weapon')
-	{
-		$('#ShieldSlot').removeClass('text-danger');
-		if(document.getElementById('2h-weaponPlaceholder')){
-			strengthToSubtract = document.getElementById('2h-weaponPlaceholder').innerHTML;
-			strengthToSubtract = strengthToSubtract.substring(strengthToSubtract.indexOf(":")+3);
-			totalStrength = parseInt(totalStrength) - parseInt(strengthToSubtract);
-			$('#ShieldSlot').text('None');
-		}
-	}
-
-
-	if(document.getElementById(itemSlot+'Placeholder')){//This slot has already been selected so we need to subtract the prior strength value
-		strengthToSubtract = document.getElementById(itemSlot+'Placeholder').innerHTML;
-		strengthToSubtract = strengthToSubtract.substring(strengthToSubtract.indexOf(":")+3);
-		totalStrength = parseInt(totalStrength) - parseInt(strengthToSubtract);
-	}
-	else{
-		//alert('It does not exist');
-	}
-	totalStrength = parseInt(totalStrength) + parseInt(strengthBonus);
-	document.getElementById('currentStrengthBonus').innerHTML=totalStrength; 
-}
-
-function addActive(diary){	
-	$('#table'+diary).removeClass('hidden');
-	$('#'+diary).addClass('active');
-	$('#'+diary).attr("onclick","hide(this.id)");
-}
-
-function hide(diary){
-	$('#table'+diary).addClass('hidden');
-	$('#'+diary).removeClass('active');
-	$('#'+diary).attr("onclick","getRequirements(this.id)");
-}
-
-function hideElement(elementId)
-{
-	$( "#table"+elementId).hide();
-
-	$( "#hidebutton"+elementId).hide();
-	$( "#showbutton"+elementId).show();
-
-}
-function showElement(element)
-{
-	$(element).removeClass('hidden');
-	$(element).show();
-}
-</script>
+<script src="maxHitScripts/maxHitFunctions.js"></script>
 
 </body>
 </html>
---
+++ maxHitScripts/maxHitFunctions.js
@@ -0,0 +1,294 @@
+function getRequirements(diary) {
+	table = "#table"+diary;
+	data ={diary: diary};
+
+	if(document.getElementById('radioChar1')){//Check if exists
+		if(document.getElementById('radioChar1').checked){
+	  		char = document.getElementById('radioChar1').value;
+			data ={diary: diary, char: char};
+		}
+	}	
+	if(document.getElementById('radioChar2')){//Check if exists
+		if(document.getElementById('radioChar2').checked){
+	  		char = document.getElementById('radioChar2').value;
+
+			data ={diary: diary, char: char};
+		}
+	}	
+	if(document.getElementById('radioChar3')){//Check if exists
+		if(document.getElementById('radioChar3').checked){
+	  		char = document.getElementById('radioChar3').value;
+			data ={diary: diary, char: char};
+		}
+	}	
+	if(document.getElementById('radioChar4')){//Check if exists
+		if(document.getElementById('radioChar4').checked){
+	  		char = document.getElementById('radioChar4').value;
+			data ={diary: diary, char: char};
+		}
+	}
+
+		$.ajax({
+			type: "POST",
+			url: "cluescroll/cluescrollRequirements.php",
+			data: data,
+			cache: false,
+
+			success: function(data) {
+			$(table).html(data);
+			},
+			error: function(xhr, ajaxOptions, thrownError) {
+				       alert(xhr.status);
+       				 alert(thrownError);
+			}
+		});
+	}
+
+function populateSlot(itemSlot,updateField) {
+	data = {itemSlot: itemSlot};
+	//updateField = '#itemSlotField';
+		$.ajax({
+			type: "POST",
+			url: "maxHitScripts/getSlotItems.php",
+			data: data,
+			cache: false,
+
+			success: function(data) {
+			$(updateField).html(data);
+			},
+			error: function(xhr, ajaxOptions, thrownError) {
+				       alert(xhr.status);
+       				 alert(thrownError);
+			}
+		});
+	}
+
+function getCharacterStrength() {
+	if (document.getElementById('radioChar1').checked) {
+		var characterName = document.getElementById('radioChar1').value;
+	}
+	else if (document.getElementById('radioChar2').checked) {
+		var characterName = document.getElementById('radioChar2').value;
+	}
+	else if (document.getElementById('radioChar3').checked) {
+		var characterName = document.getElementById('radioChar3').value;
+	}
+	else if (document.getElementById('radioChar4').checked) {
+		var characterName = document.getElementById('radioChar4').value;
+	}
+	
+	data = {characterName: characterName};
+	updateField = '#strengthLevelDiv';
+		$.ajax({
+			type: "POST",
+			url: "maxHitScripts/maxHitQueries.php",
+			data: data,
+			cache: false,
+
+			success: function(data) {
+			$(updateField).html(data);
+			},
+			error: function(xhr, ajaxOptions, thrownError) {
+				       alert(xhr.status);
+       				 alert(thrownError);
+			}
+		});
+	}
+
+function calculateMaxHit(){
+
+	strengthBonus = parseInt(document.getElementById('currentStrengthBonus').innerHTML);
+	strengthLevel = parseInt(document.getElementById('strengthLevel').value);
+	boost = document.getElementById('boost').value;
+	prayer = document.getElementById('prayer').value;
+	attackStyle = document.getElementById('attackStyle').value;
+	setBonus = parseFloat('1.00');
+	strengthLevelBool = false;
+	attackStyleBool = false;
+	specialAttack = parseFloat('1.00');
+
+	if(strengthLevel==''){
+		document.getElementById('strengthLevelMessage').innerHTML = 'Please enter a number 1-99';
+	}
+	else
+	{
+		if(isNaN(strengthLevel)){
+			document.getElementById('strengthLevelMessage').innerHTML = 'Please enter a number 1-99';
+		}	
+		else
+		{
+			if((strengthLevel > 0)&&(strengthLevel < 100)){//Valid Strength level
+				document.getElementById('strengthLevelMessage').innerHTML = "";
+				strengthLevelBool = true;
+			}
+			else{
+				document.getElementById('strengthLevelMessage').innerHTML = 'Strength must be between 1-99';
+			}
+		}
+	}
+
+	if(attackStyle =="Select style"){
+		$('#attackStyleMessage').removeClass('hidden');
+	}
+	else{
+		$('#attackStyleMessage').addClass('hidden');
+		attackStyleBool = true;
+		
+	}
+
+	if((attackStyleBool)&&(strengthLevelBool))//Good to calculate the max hit
+	{		
+		//Calculate potion bonus
+		if(boost=="None"){
+			boost = parseFloat('0.00');
+		}
+		else if((boost=="Strength potion")||(boost=="Combat potion")){
+			boost = parseFloat(strengthLevel*0.10)+parseFloat(3);
+		}
+		else if((boost=="Super strength potion")||(boost=="Super combat potion")||(boost=="Overload potion(Nightmare Zone)")){
+			boost = parseFloat(strengthLevel*0.15)+parseFloat(5);
+		}
+		else if(boost=="Zamorak brew"){
+		boost = parseFloat(strengthLevel*0.12)+parseFloat(2);
+		}
+		else if(boost=="Overload potion(-)"){
+			boost = parseFloat(strengthLevel*0.10)+parseFloat(4);
+		}
+		else if(boost=="Overload potion(Chambers of Xeric)"){
+			boost = parseFloat(strengthLevel*0.13)+parseFloat(5);
+		}
+		else if(boost=="Overload potion(+)"){
+			boost = parseFloat(strengthLevel*0.16)+parseFloat(6);
+		}
+		//alert('Boost is: '+ boost);
+			boost = parseInt(Math.floor(boost));
+		//alert('Rounded down boost is: '+ boost);
+
+		switch(attackStyle){//Convert attackStyle to integer value
+			case 'Accurate':
+			attackStyle = parseInt('0');
+			break;
+			case 'Aggressive':
+			attackStyle = parseInt('3');
+			break;
+			case 'Controlled':
+			attackStyle = parseInt('1');
+			break;
+			case 'Defensive':
+			attackStyle = parseInt('0');
+			break;
+		}
+
+		switch(prayer){//Convert prayer bonus to integer value
+			case 'None':
+			prayer = parseFloat('1.00');
+			break;
+			case 'Burst of Strength':
+			prayer = parseFloat('1.05');
+			break;
+			case 'Superhuman Strength':
+			prayer = parseFloat('1.10');
+			break;
+			case 'Ultimate Strength':
+			prayer = parseFloat('1.15');
+			break;
+			case 'Chivalry':
+			prayer = parseFloat('1.18');
+			break;
+			case 'Piety':
+			prayer = parseFloat('1.23');
+			break;
+		}		
+
+		//Calculate effective strength
+		effectiveStrength = parseFloat(Math.floor((strengthLevel+boost)*prayer*setBonus)+attackStyle);
+
+		//Calculate the base damage
+		baseDamage= 1.3+(effectiveStrength/10)+(strengthBonus/80)+((effectiveStrength*strengthBonus)/640);
+		maxHit= Math.floor(baseDamage);
+		document.getElementById('currentMaxHit').innerHTML=maxHit;//Update max hit element
+		//Calculate bonus damage if using as special attack
+		//maxHit = Math.floor(baseDamage*specialAttack);
+	}
+
+}
+
+function updateTotalStrength(strengthBonus, itemSlot) {//Needs to be called after each item is added and completely recalculated in-case items are rechosen
+	totalStrength = document.getElementById('currentStrengthBonus').innerHTML;
+
+	if(itemSlot=='2h-weapon')//Cannot have a shield with a 2h weapon
+	{
+		if(document.getElementById('WeaponPlaceholder')){
+			strengthToSubtract = document.getElementById('WeaponPlaceholder').innerHTML;
+			strengthToSubtract = strengthToSubtract.substring(strengthToSubtract.indexOf(":")+3);
+			totalStrength = parseInt(totalStrength) - parseInt(strengthToSubtract);
+		}
+		if(document.getElementById('ShieldPlaceholder')){
+			subtractShieldBonus = document.getElementById('ShieldPlaceholder').innerHTML;
+			subtractShieldBonus = subtractShieldBonus.substring(subtractShieldBonus.indexOf(":")+3);
+			totalStrength = parseInt(totalStrength) - parseInt(subtractShieldBonus);
+		}
+		$('#ShieldSlot').text('Unavailable: 2 handed weapon');
+		$('#ShieldSlot').addClass('text-danger');
+	}
+
+	if(itemSlot=='Shield')//Cannot have a 2h weapon with a shield
+	{
+		$('#ShieldSlot').removeClass('text-danger');
+		if(document.getElementById('2h-weaponPlaceholder')){
+			subtract2HandedBonus = document.getElementById('2h-weaponPlaceholder').innerHTML;
+			subtract2HandedBonus = subtract2HandedBonus.substring(subtract2HandedBonus.indexOf(":")+3);
+			totalStrength = parseInt(totalStrength) - parseInt(subtract2HandedBonus);
+			$('#WeaponSlot').text('None');
+		}
+	}
+	if(itemSlot=='Weapon')
+	{
+		$('#ShieldSlot').removeClass('text-danger');
+		if(document.getElementById('2h-weaponPlaceholder')){
+			strengthToSubtract = document.getElementById('2h-weaponPlaceholder').innerHTML;
+			strengthToSubtract = strengthToSubtract.substring(strengthToSubtract.indexOf(":")+3);
+			totalStrength = parseInt(totalStrength) - parseInt(strengthToSubtract);
+			$('#ShieldSlot').text('None');
+		}
+	}
+
+
+	if(document.getElementById(itemSlot+'Placeholder')){//This slot has already been selected so we need to subtract the prior strength value
+		strengthToSubtract = document.getElementById(itemSlot+'Placeholder').innerHTML;
+		strengthToSubtract = strengthToSubtract.substring(strengthToSubtract.indexOf(":")+3);
+		totalStrength = parseInt(totalStrength) - parseInt(strengthToSubtract);
+	}
+	else{
+		//alert('It does not exist');
+	}
+	totalStrength = parseInt(totalStrength) + parseInt(strengthBonus);
+	document.getElementById('currentStrengthBonus').innerHTML=totalStrength; 
+}
+
+function addActive(diary){	
+	$('#table'+diary).removeClass('hidden');
+	$('#'+diary).addClass('active');
+	$('#'+diary).attr("onclick","hide(this.id)");
+}
+
+function hide(diary){
+	$('#table'+diary).addClass('hidden');
+	$('#'+diary).removeClass('active');
+	$('#'+diary).attr("onclick","getRequirements(this.id)");
+}
+
+function hideElement(elementId)
+{
+	$( "#table"+elementId).hide();
+
+	$( "#hidebutton"+elementId).hide();
+	$( "#showbutton"+elementId).show();
+
+}
+function showElement(element)
+{
+	$(element).removeClass('hidden');
+	$(element).show();
+}
+
