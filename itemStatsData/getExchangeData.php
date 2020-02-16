f6da6084b9cbdc253fd982a67e18daec895a1752
Ricky <rlc9@njit.edu>
2020-02-16 17:16:12 -0500

    Removing unused pages

--- itemStatsData/getExchangeData.php
+++ itemStatsData/getExchangeData.php
@@ -44,72 +44,64 @@ $itemList = array_chunk($itemListArray,10);
 
 //Execute insert/update
 $conn = connectToDb();
-/*
-+-----------------+-------------+------+-----+---------+-------+
-| Field           | Type        | Null | Key | Default | Extra |
-+-----------------+-------------+------+-----+---------+-------+
-| itemID          | int(5)      | NO   | PRI | NULL    |       |
-| name            | varchar(60) | YES  |     | NULL    |       |
-| members         | tinyint(1)  | YES  |     | NULL    |       |
-| shopPrice       | int(50)     | YES  |     | NULL    |       |
-| buyAverage      | int(50)     | YES  |     | NULL    |       |
-| buyQuantity     | int(50)     | YES  |     | NULL    |       |
-| sellAverage     | int(50)     | YES  |     | NULL    |       |
-| sellQuantity    | int(50)     | YES  |     | NULL    |       |
-| overallAverage  | int(50)     | YES  |     | NULL    |       |
-| overallQuantity | int(60)     | YES  |     | NULL    |       |
-+-----------------+-------------+------+-----+---------+-------+*/
+
+$stmt=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
+ON DUPLICATE KEY UPDATE
+buyQuantity=?,
+sellQuantity=?,
+overallQuantity=?
+");
+
+$stmt2=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
+ON DUPLICATE KEY UPDATE
+buyAverage=?,
+buyQuantity=?,
+sellQuantity=?,
+overallAverage=?,
+overallQuantity=?
+");
+
+$stmt3=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
+ON DUPLICATE KEY UPDATE
+buyQuantity=?,
+sellAverage=?,
+sellQuantity=?,
+overallAverage=?,
+overallQuantity=?
+");
+
+$stmt4=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
+ON DUPLICATE KEY UPDATE
+buyAverage=?,
+buyQuantity=?,
+sellAverage=?,
+sellQuantity=?,
+overallAverage=?,
+overallQuantity=?
+");
 
 foreach($itemList as $item){
 
 //Case update neither buy nor sell
 if(($item[5]==0)&&($item[7]==0)){
-	$stmt=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
-	ON DUPLICATE KEY UPDATE
-	buyQuantity=?,
-	sellQuantity=?,
-	overallQuantity=?
-	");
 	$stmt->bind_param('isiiiiiiiiiii',$item[0],$item[1],$item[2],$item[3],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9],$item[5],$item[7],$item[9]);
+	$stmt->execute();
 }
 //Case update only buy
 elseif($item[7]==0){
-	$stmt=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
-	ON DUPLICATE KEY UPDATE
-	buyAverage=?,
-	buyQuantity=?,
-	sellQuantity=?,
-	overallAverage=?,
-	overallQuantity=?
-	");
-	$stmt->bind_param('isiiiiiiiiiiiii',$item[0],$item[1],$item[2],$item[3],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9],$item[4],$item[5],$item[7],$item[8],$item[9]);
+	$stmt2->bind_param('isiiiiiiiiiiiii',$item[0],$item[1],$item[2],$item[3],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9],$item[4],$item[5],$item[7],$item[8],$item[9]);
+	$stmt2->execute();
 }
 //Case update only sell
 elseif($item[5]==0){
-	$stmt=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
-	ON DUPLICATE KEY UPDATE
-	buyQuantity=?,
-	sellAverage=?,
-	sellQuantity=?,
-	overallAverage=?,
-	overallQuantity=?
-	");
-	$stmt->bind_param('isiiiiiiiiiiiii',$item[0],$item[1],$item[2],$item[3],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9],$item[5],$item[6],$item[7],$item[8],$item[9]);
+	$stmt3->bind_param('isiiiiiiiiiiiii',$item[0],$item[1],$item[2],$item[3],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9],$item[5],$item[6],$item[7],$item[8],$item[9]);
+	$stmt3->execute();
 }
 //Case update all
 else{
-	$stmt=$conn->prepare("INSERT INTO exchange VALUES (?,?,?,?,?,?,?,?,?,?)
-	ON DUPLICATE KEY UPDATE
-	buyAverage=?,
-	buyQuantity=?,
-	sellAverage=?,
-	sellQuantity=?,
-	overallAverage=?,
-	overallQuantity=?
-	");
-	$stmt->bind_param('isiiiiiiiiiiiiii',$item[0],$item[1],$item[2],$item[3],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9]);
+	$stmt4->bind_param('isiiiiiiiiiiiiii',$item[0],$item[1],$item[2],$item[3],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9],$item[4],$item[5],$item[6],$item[7],$item[8],$item[9]);
+	$stmt4->execute();
 }
-$stmt->execute();
 
 }
 mysqli_close($conn);
